ac68fd7040be4c1b428ec4d9c0e816a9a47d730a
mekuannent-biyazn <mekuannentbiyazn@gmail.com>
2026-06-04 14:52:10 +0300

    fiat: I done supportive project for opperators, conditional statments and switch statement

--- Day-One/operators.go
+++ Day-One/operators.go
@@ -83,4 +83,85 @@ func main(){
 	default:
 		fmt.Println("Invalid day of the week.")
 	}
+
+	// login validation
+	var username, password string
+	fmt.Print("Enter username: ")
+	fmt.Scanln(&username)
+	fmt.Print("Enter password: ")
+	fmt.Scanln(&password)
+
+	if username == "admin" && password == "password" {
+		fmt.Println("Login successful!")
+	} else {
+		fmt.Println("Invalid username or password.")
+	}
+
+	// user acess level
+	role := "admin"
+
+	switch role {
+	case "admin":
+		fmt.Println("you have full access")
+	case "editor":
+		fmt.Println("you have edit access")
+	case "viewer":
+		fmt.Println("you have view access")
+	default:
+		fmt.Println("invalid role")
+	}
+
+	// student grade evaluation
+	var grade float64
+	fmt.Print("Enter your grade: ")
+	fmt.Scanln(&grade)
+
+	if grade >= 90 && grade <= 100 {
+		fmt.Println("You got an A.")
+	} else if grade >= 80 && grade < 90 {
+		fmt.Println("You got a B.")
+	} else if grade >= 70 && grade < 80 {
+		fmt.Println("You got a C.")
+	} else if grade >= 60 && grade < 70 {
+		fmt.Println("You got a D.")
+	} else {
+		fmt.Println("You got an F.")
+	}
+
+	// Employee Bonus System
+	var salary float64
+	fmt.Print("Enter your salary: ")
+	fmt.Scanln(&salary)
+
+	if salary >= 20000{
+		salary =salary*0.2 + salary
+		fmt.Println("You get a 20% bonus. Your new salary is", salary)
+	} else if salary >= 10000 && salary < 20000 {
+		salary = salary*0.1 + salary
+		fmt.Println("You get a 10% bonus. Your new salary is", salary)
+	} else {
+		salary = salary*0.05 + salary
+		fmt.Println("You get a 5% bonus. Your new salary is", salary)
+	}
+
+	// Student Result Management Program:
+	var Sname string
+	var Score float64
+	
+	fmt.Print("Enter student name: ")
+	fmt.Scanln(&Sname)
+	fmt.Print("Enter student score: ")
+	fmt.Scanln(&Score)
+
+	if Score >= 90 && Score <= 100 {
+		fmt.Println("Student Name: ",Sname, "\nScore :", Score, "\nGrade : A", "\nstatus: Pass")
+	} else if Score >= 80 && Score < 90 {
+		fmt.Println("Student Name: ",Sname, "\nScore :", Score, "\nGrade : B", "\nstatus: Pass")
+	} else if Score >= 70 && Score < 80 {
+		fmt.Println("Student Name: ",Sname, "\nScore :", Score, "\nGrade : C", "\nstatus: Pass")
+	} else if Score >= 60 && Score < 70 {
+		fmt.Println("Student Name: ",Sname, "\nScore :", Score, "\nGrade : D", "\nstatus: Pass")
+	} else {
+		fmt.Println("Student Name: ",Sname, "\nScore :", Score, "\nGrade : F", "\nstatus: Fail")
+	}
 }
