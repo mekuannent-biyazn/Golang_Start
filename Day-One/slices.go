1dfc33e4e5c4d0ed51d6b403570f1ffcdbd6000e
mekuannent-biyazn <mekuannentbiyazn@gmail.com>
2026-06-05 22:31:47 +0300

    fiat: add some practical mini project to advance the slice know how

--- Day-One/slices.go
+++ Day-One/slices.go
@@ -31,5 +31,36 @@ func main(){
 
 	// Slice Internals
 	fmt.Println(len(number))
-	fmt.Println(cap(number))
+	fmt.Println(cap(number)) 
+
+	Mksl:= make([]int, 0, 20)
+	Mksl= append(Mksl, 10, 20, 30)
+
+	fmt.Println(Mksl, len(Mksl), cap(Mksl))
+
+	// coping slices
+	src:= []int {1,2,3}
+	dest:= make([]int, len(src))
+	copy(dest, src)
+	dest = append(dest, 4,5,6)
+	fmt.Println("The source slice is : ",src)
+	fmt.Println("The destination slice is : ",dest)
+
+	prts:= []int {10, 20, 30, 40, 50}
+	for _, value:= range prts{
+		fmt.Println(value)
+	}
+
+	var n int
+	fmt.Print("Enter required number of students: ")
+	fmt.Scanln(&n)
+	Stname:= [] string {}
+	var name string
+	for i:=0; i<=n; i++{
+		fmt.Printf("Enter student %d ", i+1)
+		fmt.Scanln(&name)
+		Stname= append(Stname, name)
+	}
+	fmt.Println(Stname)
+	fmt.Println(Stname[1:4])
 }
