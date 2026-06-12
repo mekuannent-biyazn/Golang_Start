f1ce5c1cd8116ad06452f472a2dad71fb3ca1d13
mekuannent-biyazn <mekuannentbiyazn@gmail.com>
2026-06-12 11:06:39 +0300

    fiat: Add http methods GET, POST, Parameter query

--- httpServer/createServer.go
+++ httpServer/createServer.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"io"
 )
 
 func Home(w http.ResponseWriter, r *http.Request){
@@ -31,10 +32,30 @@ func Methodvalidator(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintln(w, "All user recoreds.")
 }
 
+func createUser(w http.ResponseWriter, r *http.Request){
+	w.WriteHeader(http.StatusCreated)
+	body, err:= io.ReadAll(r.Body)
+
+	if err!= nil{
+		http.Error(w, "Failed!", http.StatusBadRequest)
+		return
+	}
+
+	fmt.Fprintln(w, "User created successfully!!\n", string(body))
+}
+
+func GerUserbyId(w http.ResponseWriter, r *http.Request){
+	id:= r.URL.Query().Get("id")
+
+	fmt.Fprintf(w, "User Id: %s", id)
+}
+
 func main(){
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
 	http.HandleFunc("/contact", Contact)
+	http.HandleFunc("/user", createUser)
+	http.HandleFunc("/users", GerUserbyId)
 	http.HandleFunc("/methodvalidator",Methodvalidator)
 	fmt.Println("Server is running on Port 8080")
 	http.HandleFunc("/methodc",Users)
