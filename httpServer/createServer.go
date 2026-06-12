990e71f455733f5b642b27311d0a5aee5ba4e705
mekuannent-biyazn <mekuannentbiyazn@gmail.com>
2026-06-12 13:05:29 +0300

    fiat: Add sending and recieving Json data

--- httpServer/createServer.go
+++ httpServer/createServer.go
@@ -3,7 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"io"
+	// "io"
+	"encoding/json"
 )
 
 func Home(w http.ResponseWriter, r *http.Request){
@@ -32,16 +33,38 @@ func Methodvalidator(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintln(w, "All user recoreds.")
 }
 
-func createUser(w http.ResponseWriter, r *http.Request){
-	w.WriteHeader(http.StatusCreated)
-	body, err:= io.ReadAll(r.Body)
+type Usersj struct{
+	Id int `json: "id"`
+	Name string `json: "name"`
+}
 
-	if err!= nil{
-		http.Error(w, "Failed!", http.StatusBadRequest)
+func createUser(w http.ResponseWriter, r *http.Request){
+	var user Usersj
+
+	erru:= json.NewDecoder(
+		r.Body,
+	).Decode(&user)
+
+	if erru!= nil {
+		http.Error(
+			w,
+			"Invalid request!",
+			http.StatusBadRequest,
+		)
 		return
 	}
+	fmt.Fprintln(w,"Id: ", user.Id)
+	fmt.Fprintln(w,"Name: ", user.Name)
 
-	fmt.Fprintln(w, "User created successfully!!\n", string(body))
+	// w.WriteHeader(http.StatusCreated)
+	// body, err:= io.ReadAll(r.Body)
+
+	// if err!= nil{
+	// 	http.Error(w, "Failed!", http.StatusBadRequest)
+	// 	return
+	// }
+
+	// fmt.Fprintln(w, "User created successfully!!\n", string(body))
 }
 
 func GerUserbyId(w http.ResponseWriter, r *http.Request){
@@ -50,11 +73,37 @@ func GerUserbyId(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "User Id: %s", id)
 }
 
+func getUsers(w http.ResponseWriter, r *http.Request){
+	users:= []Usersj{
+		{1, "mekuannent"},
+		{2, "Tirusiet"},
+	}
+
+	jsonData,_:= json.MarshalIndent(
+		users,
+		"",
+		" ",
+	) 
+
+	w.Header().Set(
+		"Content-Type",
+		"application/json",
+	)
+
+	user1:= Usersj{
+		Id: 3,
+		Name: "Hiemu",
+	}
+	json.NewEncoder(w).Encode(user1)
+	fmt.Fprintln(w,"All Registerd Users Are!\n", string(jsonData))
+}
+
 func main(){
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
 	http.HandleFunc("/contact", Contact)
 	http.HandleFunc("/user", createUser)
+	http.HandleFunc("/users/all",getUsers)
 	http.HandleFunc("/users", GerUserbyId)
 	http.HandleFunc("/methodvalidator",Methodvalidator)
 	fmt.Println("Server is running on Port 8080")
