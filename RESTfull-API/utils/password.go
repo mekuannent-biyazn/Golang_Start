0f0b28da38be2f60b6d11071617e13aaeb88ffe7
mekuannent-biyazn <mekuannentbiyazn@gmail.com>
2026-06-15 14:22:32 +0300

    fiat: Hashing the password for security

---
+++ RESTfull-API/utils/password.go
@@ -0,0 +1,25 @@
+package utils
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(
+	password string,
+) (string, error) {
+
+	hash, err :=
+		bcrypt.GenerateFromPassword(
+			[]byte(password),
+			bcrypt.DefaultCost,
+		)
+
+	return string(hash), err
+}
+
+func CheckPassword(hashedPassword, password string) error{
+	return bcrypt.CompareHashAndPassword(
+		[]byte(hashedPassword),
+		[]byte(password),
+	)
+}
