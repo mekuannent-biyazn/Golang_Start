3ec8ea36d42105eb572ce42e2ac93c01004a859a
mekuannent-biyazn <mekuannentbiyazn@gmail.com>
2026-06-16 15:26:58 +0300

    fiat: Add some routes

--- RESTfull-API/main.go
+++ RESTfull-API/main.go
@@ -5,12 +5,17 @@ import (
 	"fmt"
 
 	"go-auth-api/handlers"
+	"go-auth-api/middleware"
 )
 
 func main(){
 	http.HandleFunc("/register", handlers.Register)
 	http.HandleFunc("/login", handlers.Login)
+	http.HandleFunc("/", handlers.GetAllUsers)
+	http.HandleFunc("/profile", middleware.AuthMiddleware(handlers.Profile))
+	http.HandleFunc("/updateprofile", middleware.AuthMiddleware(handlers.UpdateProfile))
+	http.HandleFunc("/deleteuser", middleware.AuthMiddleware(handlers.DeleteUser))
 
-	http.Fprintln("server is running on localhost:5050")
+	fmt.Println("server is running on localhost:5050")
 	http.ListenAndServe(":5050", nil)
 }
