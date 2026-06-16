4ad5796e645ef255cd42b9794f72519b6ca0d599
mekuannent-biyazn <mekuannentbiyazn@gmail.com>
2026-06-16 15:24:22 +0300

    fiat: Add generate and validate token functionality

--- RESTfull-API/utils/jwt.go
+++ RESTfull-API/utils/jwt.go
@@ -11,12 +11,25 @@ func GenerateToken(userID int)(string, error){
 	token:= jwt.NewWithClaims(
 		jwt.SigningMethodHS256,
 		jwt.MapClaims{
-			"user-ID": userID,
-			"exp": time.Now().Add(5*time.Hour).Unix(),
+			"user_id": userID,
+			"exp": time.Now().Add(24*time.Hour).Unix(),
 		},
 	)
 
 	return token.SignedString(
 		SecretKey,
+	)
+}
+
+func ValidateToken(
+	tokenString string,
+) (*jwt.Token, error) {
+
+	return jwt.Parse(
+		tokenString,
+		func(token *jwt.Token) (interface{}, error) {
+
+			return SecretKey, nil
+		},
 	)
 }
